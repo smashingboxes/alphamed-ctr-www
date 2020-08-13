5e9f86e8515ad20f37cb83b0a7db4f55d8c46262
ianlavapiez <ianlavapiez@gmail.com>
2020-08-13 18:46:42 +0800

    refactor: changed the javascript folder structure and base layout html file application

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,10 +1,10 @@
 class ResultsController < ApplicationController
 
-  def index
-    @role = current_user ? current_user[:role] : "guest"
-    @regulars = User.regular
-    @admins = User.admin
-    @editors = User.editor
-    @co_authors = User.co_author
+    def index
+      @role = current_user ? current_user[:role] : "guest"
+      @regulars = User.regular
+      @admins = User.admin
+      @editors = User.editor
+      @co_authors = User.co_author
+    end
   end
-end
