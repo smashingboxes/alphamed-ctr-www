e6b1d0acc43b91d1cc19415a068d046161c75cf7
Nico Ricafrente <nicokokun@yahoo.com>
2020-08-13 21:34:24 +0800

    Created APIs for both User Login and User Registration.

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,36 @@
+class Api::SessionsController < DeviseController
+  prepend_before_action  :require_no_authentication, :only => [:create ]
+  skip_before_action :verify_authenticity_token, only: [:create]
+  
+  before_action :ensure_params_exist
+
+  respond_to :json
+  
+  def create
+    # build_resource
+    resource = User.find_for_database_authentication(:email=>params[:user_login][:email])
+    return invalid_login_attempt unless resource
+
+    if resource.valid_password?(params[:user_login][:password])
+      sign_in("user", resource)
+      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :email=>resource.email}
+      return
+    end
+    invalid_login_attempt
+  end
+  
+  def destroy
+    sign_out(resource_name)
+  end
+
+  protected
+  def ensure_params_exist
+    return unless params[:user_login].blank?
+    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
+  end
+
+  def invalid_login_attempt
+    warden.custom_failure!
+    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
 
+  namespace :api do
+    devise_for :users do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+    resources :recipes, :only=>[:index, :show]
+  end  
+  
   resources :results, as: "published_result"
   root to: "results#index"
 end
