e527ef1907c4da9ce58456ebd7240fa07401a644
ianlavapiez <ianlavapiez@gmail.com>
2020-08-13 21:43:28 +0800

    refactor: changed the global styles css

--- app/javascript/packs/global.styles.jsx
+++ app/javascript/packs/global.styles.jsx
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     html {
-        font-size: 62.5%;
+        font-size: 16px;
     }
 
     body {
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { GlobalStyle } from '../packs/global.styles';
+import PrimaryButton from './components/shared/primary-button/primary-button.component';
+import SecondaryButton from './components/shared/secondary-button/secondary-button.component';
+import ErrorButton from './components/shared/error-button /error-button.component';
 
 const App = () => {
+  const handleClick = () => alert('hello');
+
   return (
     <div>
       <GlobalStyle />
-      Hello World
+      <PrimaryButton onClick={handleClick}>Primary Button</PrimaryButton>
+      <SecondaryButton onClick={handleClick}>Secondary Button</SecondaryButton>
+      <ErrorButton onClick={handleClick}>Error Button</ErrorButton>
     </div>
   );
 };
