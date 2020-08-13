d91fcbdbc033f1780e3776f9cb0000d99080ac4c
Nico Ricafrente <nicokokun@yahoo.com>
2020-08-13 21:32:52 +0800

    Added method 'generate_token' to generate an auth token for user when it is initally created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: {regular: 0, admin: 1, editor: 2, co_author: 3}
+
+  before_create :generate_token
+
+  protected
+
+  def generate_token
+    self.authentication_token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless User.exists?(authentication_token: random_token)
+    end
+  end
 end
