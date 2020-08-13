fabc42188865aa968304463a9cf5e0286ed0a171
Nico Ricafrente <nicokokun@yahoo.com>
2020-08-13 21:34:39 +0800

    Updated Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_120328) do
+ActiveRecord::Schema.define(version: 2020_08_13_130425) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_120328) do
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
     t.string "name"
+    t.string "authentication_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
