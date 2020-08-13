09775fb15f39ca531e7bc5dddf629a5539518b1c
ianlavapiez <ianlavapiez@gmail.com>
2020-08-13 20:44:59 +0800

    feat: added global styles and assets folder

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { GlobalStyle } from '../packs/global.styles';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <GlobalStyle />
+      Hello World
+    </div>
+  );
 };
 
 export default App;
