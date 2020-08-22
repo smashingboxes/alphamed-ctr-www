8e35bfbcdbad0df4e3cec38c942d0e3cd5e2f2af
Nico Ricafrente <nicoricafrente@gmail.com>
2020-08-22 17:16:37 +0800

    feat: Changed User Edit API because of integrating MongoDB.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,29 @@
 Rails.application.routes.draw do
 
+  
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    post '/users/sign_in' => 'sessions#create'
+    # post '/users/sign_up' => 'registrations#create'
+  end
+
+  resources :roles
+  resources :comments
+  resources :emails
+  resources :results
+  resources :posts
   namespace :api do
-    devise_for :users do
-      get '/users/sign_out' => 'devise/sessions#destroy'
-    end
+    # devise_for :users, excepted: %w['sessions#new session#destroy'] do
+    #   get '/users/sign_out' => 'devise/sessions#destroy'
+    # end
     resources :users, only: [] do
       collection do
         post 'update', to: 'users#update'
+        # post 'sign_in', to: 'devise/sessions#create'
       end
     end
   end  
-  
+
   resources :results, as: "published_result"
   root to: "results#index"
 end
