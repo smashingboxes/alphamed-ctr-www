b3c772da5341c327cccafb392d64d770449e740e
Nico Ricafrente <nicokokun@yahoo.com>
2020-09-29 22:15:20 +0800

    fix: Fixed issue with Heroku deployment.

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative 'boot'
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_cable"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
